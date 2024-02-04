2c6ad2e15b188a6ed3afa6286d85722795a2159e
pro-masud <promasudbd@gmail.com>
2024-02-05 00:26:30 +0600

    node js developer package modules add successfully complete and working successfully

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const {info, age} = require('./src/functions');
 const devsData = require('./src/data');
 const pro = require('./src/Class');
 const fs = require('fs');
+const colors = require('colors');
 
 const data = fs.readFileSync('./data/olddata.txt').toString();
 
-fs.writeFile('nodebook.txt', data, () => {
-    
-});
 
+// fs.writeFile('nodebook.txt', data, () => {
+
+// });
+console.log(`we are batch 2024`.bgMagenta.black);
